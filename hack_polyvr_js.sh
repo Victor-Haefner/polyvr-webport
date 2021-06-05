22391723088f8a4fa46e221fd675bc99805af3a2
Victor Haefner <victor.haefner@gmail.com>
2021-06-05 10:14:26 +0300

    working on editor

--- hack_polyvr_js.sh
+++ hack_polyvr_js.sh
@@ -5,3 +5,4 @@
 sed '/function getBinaryPromise() {/a if (storage.ressource) {\n\tconsole.log("use stored wasm binary!");\n\treturn new Promise(function(resolve, reject) { resolve(storage.ressource); });\n}' -i polyvr.js
 sed 'N;/function instantiateAsync() {\n    if (!wasmBinary &&/a !storage.ressource &&' -i polyvr.js
 sed 's/window.addEventListener/Module["canvas"].addEventListener/g' -i polyvr.js
+sed 's/if (buffer.buffer === HEAP8.buffer) {/if (HEAP8 \&\& buffer.buffer === HEAP8.buffer) {/g' -i polyvr.js
